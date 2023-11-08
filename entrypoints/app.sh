648906e91184951bc4dbdb657b274bd963a33be0
Alishba Saeed <alishbasaeed7109@gmail.com>
2023-11-08 03:46:40 -0800

    Docker update for production

--- entrypoints/app.sh
+++ entrypoints/app.sh
@@ -5,4 +5,7 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py makemigrations superadmin
 python manage.py migrate
+# this for development
 exec python manage.py runserver 0.0.0.0:3001
+# this is for production
+#gunicorn backend.wsgi:application --bind 0.0.0.0:3001
